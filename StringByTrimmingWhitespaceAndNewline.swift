6192cc052d2f6f2e9414d65ae0127784c5d514de
Wyszo <wyszo@Admins-MacBook-Pro.local>
2015-12-27 10:17:22 +0100

    feat: Common stringByTrimmingWhitespace moved to a file with shared unit tests

--- StringByTrimmingWhitespaceAndNewline.swift
+++ StringByTrimmingWhitespaceAndNewline.swift
@@ -1,16 +1,16 @@
 import XCTest
 
-class StringByTrimmingWhitespaceTests : StringByTrimmingSharedTests {
+class StringByTrimmingWhitespaceAndNewline : StringByTrimmingSharedTests {
 
     // TODO: this needs to be passed as a pointer to function, without a need to override the method!
     override func trimmingMethod(string : String) -> String? {
         // I should use a pointer to a function in here instead!!
-        return string.stringByTrimmingWhitespace()
+        return string.stringByTrimmingWhitespaceAndNewline()
     }
 
     func testNewlineNotTrimming() {
         let aWordAndNewline = "Champ \n"
         let trimmedString = trimmingMethod(aWordAndNewline)
-        XCTAssertEqual("Champ \n", trimmedString, "string containing a word and a newline should not change after trimming whitespace")
+        XCTAssertEqual("Champ", trimmedString, "string containing a word and a newline should return just the word after trimming whitespace and newline")
     }
 }
